fce0325581e955358f5ab4bef79b40a611b3f7ba
Carolina Huergo <carohuergo@outlook.com>
2023-10-22 11:39:02 -0300

    grabo primer prueba

--- src/Componentes/Informacion/Informacion.jsx
+++ src/Componentes/Informacion/Informacion.jsx
@@ -10,20 +10,20 @@ export const Informacion = () => {
     return (
         <section className="ceremonia-fiesta">
             <div className="container">
-                <Row>
+                <Row style={{justifyContent:"center"}}>
                     <div className="col-md-6 columna">
                         <img style={{ height: "15rem" }} src="https://agendalafecha.com/demolepou/assets/img/icono-ceremonia.svg" alt="" className="icon"></img>
-                        <h4>Ceremonia</h4>
-                        <p>26 de Agosto a las 16:30 horas en la Capilla Santa Teresita.</p>
-                        <p> Golf Club Golfers 2911, Garin, Buenos Aires. </p>
-                        <p>No llegues tarde! La novia es muy puntual!</p>
-                        <a target="_blank" rel="noopener noreferrer" href="https://goo.gl/maps/JJSDDcMjrenVqJou8">
+                        <h4>Ceremonia y Fiesta</h4>
+                        <p>9 de Diciembre a las 18:30 horas en Rut Haus.</p>
+                        <p> Av. Int. Cantilo 1429, CABA. </p>
+                        <p>No llegues tarde! No queremos empenzar sin vos!</p>
+                        <a target="_blank" rel="noopener noreferrer" href="https://maps.app.goo.gl/vT6Rp5kWTjxu4DdV8">
                             <Button className="btn">LLEGAR A LA CEREMONIA</Button>
                         </a>
                     </div>
 
 
-                    <div className="col-md-6 columna">
+                    {/* <div className="col-md-6 columna">
                         <img style={{ height: "15rem" }} src="https://agendalafecha.com/demolepou/assets/img/icono-fiesta.svg" alt="" className="icon"></img>
                         <h4>Fiesta</h4>
                         <p>Te esperamos despues de la ceremonia en Arena Golf.</p>
@@ -32,7 +32,7 @@ export const Informacion = () => {
                         <a target="_blank" rel="noopener noreferrer" href="https://goo.gl/maps/aDbaVbqfjTau34u87">
                             <Button className="btn">LLEGAR A LA FIESTA</Button>
                         </a>
-                    </div>
+                    </div> */}
                 </Row>
             </div>
         </section>
--- src/Componentes/portada/portada.jsx
+++ src/Componentes/portada/portada.jsx
@@ -10,15 +10,15 @@ export const Portada = () => {
       <> 
       <section>
       <ParallaxBanner
-        layers={[{ image: 'https://firebasestorage.googleapis.com/v0/b/ecommerce-carolahome.appspot.com/o/IMG_0848.jpg?alt=media&token=2e0b7ee1-1d7a-4641-9740-0028493f3713', speed: -20}]}
+        layers={[{ image: 'https://firebasestorage.googleapis.com/v0/b/ecommerce-carolahome.appspot.com/o/ellos2.jpeg?alt=media&token=c2cdf1a9-5830-4c98-afaf-19f5364dfad8&_gl=1*hqenfe*_ga*MTQ0NDI2MTgwMi4xNjg0ODc5MjE5*_ga_CW55HF8NVT*MTY5Njk4MDcxOS4xOS4xLjE2OTY5ODA5MDguMy4wLjA.', speed: -20,}]}
         className="portadaFondo"
         
       >
       
         <div style={{ position:"absolute", display:"flex", justifyContent:"flex-end", alignItems:"center", flexDirection:"column"}} className="absolute inset-0 flex items-center justify-center">
           <div className="fondoTitulo">
-          <h1 className="text-8xl text-6xl text-white font-thin ">GERO & CARO</h1>
-          <h2 className=" text-white font-thin ">26.08.2023</h2>
+          <h1 className="text-8xl text-6xl text-white font-thin ">Silvina & Baby</h1>
+          <h2 className=" text-white font-thin ">09.12.2023</h2>
           </div>
           
         </div>
